251bea5e4e62fab829f8ea51e9725674a7628f0d
Nafi R <saejim91@gmail.com>
2023-06-21 00:07:59 +0600

    responsive implemented

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,25 +3,37 @@ import { NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
-  return (
-    <header className={classes.header}>
-      <div className={classes.logo}>QStack</div>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <NavLink to='/quotes' className={navData => navData.isActive ? classes.active : '' }>
-              All Quotes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/new-quote' className={navData => navData.isActive ? classes.active : '' }>
-              Add a Quote
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={classes.header}>
+			<div className={classes.wrapper}>
+				<div className={classes.logo}>QStack</div>
+				<nav className={classes.nav}>
+					<ul>
+						<li>
+							<NavLink
+								to="/quotes"
+								className={(navData) =>
+									navData.isActive ? classes.active : ''
+								}
+							>
+								All Quotes
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to="/new-quote"
+								className={(navData) =>
+									navData.isActive ? classes.active : ''
+								}
+							>
+								Add a Quote
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default MainNavigation;
