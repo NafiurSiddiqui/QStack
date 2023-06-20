8df7c908fb6b4e9a1a1341d249f3279594b8607c
Nafi R <saejim91@gmail.com>
2023-06-20 20:38:40 +0600

    filtered words added

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -4,84 +4,90 @@ import { useRef, useState } from 'react';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
+import filteredWords from '../../lib/filteredWords';
 
 const QuoteForm = (props) => {
-  const [isEntering, setIsEntering] = useState(false);
-  const [btnDisable, setBtnDisable] = useState(true);
+	const [isEntering, setIsEntering] = useState(false);
+	const [btnDisable, setBtnDisable] = useState(true);
 
-  const authorInputRef = useRef();
-  const textInputRef = useRef();
+	const authorInputRef = useRef();
+	const textInputRef = useRef();
 
-  function submitFormHandler(event) {
-    event.preventDefault();
+	function submitFormHandler(event) {
+		event.preventDefault();
 
-    const enteredAuthor = authorInputRef.current.value;
-    const enteredText = textInputRef.current.value;
+		const enteredAuthor = authorInputRef.current.value;
+		const enteredText = textInputRef.current.value;
 
-    //validation
-    if(enteredAuthor.length === 0 ){
-      setBtnDisable(true);
-      alert('Don\'t your favourite author deserves some credentials?');     
-      return;
-    }
-    
-    if(enteredAuthor.length === 0 ){
-      setBtnDisable(true);
-      alert('Don\'t your favourite author deserves some credentials? :/');     
-      return;
-    }
+		//validation
+		if (enteredAuthor.length === 0) {
+			setBtnDisable(true);
+			alert("Don't your favourite author deserves some credentials?");
+			return;
+		}
 
-    if(enteredText.length === 0 ){
-      setBtnDisable(true);
-      alert('Your favorite author quoted nothing? :/');     
-      return;
-    }
+		if (enteredAuthor.length === 0) {
+			setBtnDisable(true);
+			alert("Don't your favourite author deserves some credentials? :/");
+			return;
+		}
 
+		if (enteredText.length === 0) {
+			setBtnDisable(true);
+			alert('Your favorite author quoted nothing? :/');
+			return;
+		}
 
-    props.onAddQuote({ author: enteredAuthor, text: enteredText });
-  }
+		props.onAddQuote({ author: enteredAuthor, text: enteredText });
+	}
 
-  const finishEnteringHandler = () => {
-    setIsEntering(false);
-    
-  };
+	const finishEnteringHandler = () => {
+		setIsEntering(false);
+	};
 
-  const formFocusedHandler = () => {
-    setIsEntering(true);  
-    setBtnDisable(false);
-    
-  };
+	const formFocusedHandler = () => {
+		setIsEntering(true);
+		setBtnDisable(false);
+	};
 
-  return (
-    <>    
-      <Card>
-        <form
-          onFocus={formFocusedHandler}
-          onBlur={()=> !isEntering && setBtnDisable(true)}
-          className={classes.form}
-          onSubmit={submitFormHandler}
-        >
-          {props.isLoading && (
-            <div className={classes.loading}>
-              <LoadingSpinner />
-            </div>
-          )}
+	//filter out and sanitize input
 
-          <div className={classes.control}>
-            <label htmlFor='author'>Author</label>
-            <input type='text' id='author' ref={authorInputRef} />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='text'>Text</label>
-            <textarea id='text' rows='5' ref={textInputRef}></textarea>
-          </div>
-          <div className={classes.actions}>
-            <button onClick={finishEnteringHandler} className='btn' disabled={btnDisable} >Add Quote</button>
-          </div>
-        </form>
-      </Card>
-    </>
-  );
+	return (
+		<>
+			<Card>
+				<form
+					onFocus={formFocusedHandler}
+					onBlur={() => !isEntering && setBtnDisable(true)}
+					className={classes.form}
+					onSubmit={submitFormHandler}
+				>
+					{props.isLoading && (
+						<div className={classes.loading}>
+							<LoadingSpinner />
+						</div>
+					)}
+
+					<div className={classes.control}>
+						<label htmlFor="author">Author</label>
+						<input type="text" id="author" ref={authorInputRef} />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="text">Text</label>
+						<textarea id="text" rows="5" ref={textInputRef}></textarea>
+					</div>
+					<div className={classes.actions}>
+						<button
+							onClick={finishEnteringHandler}
+							className="btn"
+							disabled={btnDisable}
+						>
+							Add Quote
+						</button>
+					</div>
+				</form>
+			</Card>
+		</>
+	);
 };
 
 export default QuoteForm;
